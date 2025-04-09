c5659b6c08345f90a69aac44a43c6bae561d2771
thuolala <phamthushame2002@gmail.com>
2025-04-09 21:57:01 +0700

    ajkabsjk

--- hanging.js
+++
@@ -1,4 +0,0 @@
-function rickRoll() {
-  const rickRollUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-  window.open(rickRollUrl, "_blank");
-}
---
+++ stuffs.js
@@ -0,0 +1,39 @@
+function rickRoll() {
+  const rickRollUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  window.open(rickRollUrl, "_blank");
+
+  openLOLModal();
+}
+
+function playAudio() {
+  let audio = document.getElementById("audio2");
+  audio.play();
+}
+
+$(document).ready(function () {
+  $(".fence").on("mouseenter", ".square", function () {
+    let audio = $("#audio1")[0];
+    audio.currentTime = 0; // Restart sound if already playing
+    audio.play();
+  });
+
+  $(".card").click(function () {
+    playAudio();
+  });
+});
+
+// LOL Modal
+function openLOLModal() {
+  $("#lolModal").modal("show"); // Show modal
+
+  // Close modal events
+  $("#closeBtn, .close").click(function () {
+      $("#lolModal").modal("hide");
+  });
+
+  $(document).on("click", function (event) {
+      if ($(event.target).hasClass("modal")) {
+          $("#lolModal").modal("hide");
+      }
+  });
+}
